61be62d4fb96a0e3a220ee3cf89e26b1249fa225
musiccity365 <m_sackstein@yahoo.com>
2020-03-11 11:02:04 -0400

    added actions and conditions to finish lab

--- lib/person.rb
+++ lib/person.rb
@@ -65,5 +65,29 @@ class Person
         return "♪ Rub-a-dub just relaxing in the tub ♫"
     end
     
+    def work_out
+        self.happiness += 2
+        self.hygiene -= 3
+        return "♪ another one bites the dust ♫"
+    end
 
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        return "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        if topic == "politics"
+            friend.happiness -= 2
+            self.happiness -= 2
+            return "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            friend.happiness += 1
+            self.happiness += 1
+            return "blah blah sun blah rain"
+        else
+            return "blah blah blah blah blah"
+        end
+    end
 end
