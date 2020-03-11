b35cd93615a04abce0ca1ea59ced723bd514d453
musiccity365 <m_sackstein@yahoo.com>
2020-03-11 10:19:15 -0400

    started happiness method

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,19 @@
 # your code goes here
+class Person
+    attr_accessor :bank_account, :happiness
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        self.happiness = 8
+    end
+    def happiness
+        if self.happiness > 10
+            happiness = 10
+        else self.happiness < 0
+            happiness = 0
+        end
+    end
+
+end
