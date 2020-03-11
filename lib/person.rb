f1c13adb4f5b1ca840e1a01d96c559e1ef42cd94
bret-gibson <bret.gibson@gmail.com>
2020-03-11 10:19:18 -0400

    Set up Personn Class to start working on get_paid

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,60 @@
-# your code goes here
+require "pry"
+class Person
+    attr_reader :name, :happiness, :hygiene
+    attr_accessor :bank_account
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(happiness)
+        if (happiness > 10)
+            @happiness = 10
+        elsif (happiness < 0)
+            @happiness = 0
+        else
+        @happiness = happiness
+        end
+    end
+
+    def hygiene=(hygiene)
+        if (hygiene > 10)
+            @hygiene = 10
+        elsif (hygiene < 0)
+            @hygiene = 0
+        else
+        @hygiene = hygiene
+        end
+    end
+
+    def happy?
+        if self.happiness > 7
+            return true
+        else
+            return false
+        end
+    end
+
+    def clean?
+        if self.hygiene > 7
+            return true
+        else 
+            return false
+        end
+    end
+  
+
+    def get_paid(amount)
+        @bank_account = self.bank_account+= amount
+        @bank_account
+       
+    
+    end
+    
+
+
+
+end
